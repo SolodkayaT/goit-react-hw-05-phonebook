dce939d44597866869e0fae5943a02524edaf1e6
SolodkayaT <tatasladkaya@gmail.com>
2020-03-30 01:23:51 +0300

    Updates

---
+++ precache-manifest.c452a1d66d48e8f0934c489571e013fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f64cfc7fcf6c562510072bd1db4fd204",
+    "url": "/goit-react-hw-05-phonebook/index.html"
+  },
+  {
+    "revision": "2b628dce1ba5c774e9ea",
+    "url": "/goit-react-hw-05-phonebook/static/css/main.1cf47e45.chunk.css"
+  },
+  {
+    "revision": "7f027ed4363a40c8a2f7",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.7feca06a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.7feca06a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b628dce1ba5c774e9ea",
+    "url": "/goit-react-hw-05-phonebook/static/js/main.9a041b4d.chunk.js"
+  },
+  {
+    "revision": "7ff03caac4a99a162586",
+    "url": "/goit-react-hw-05-phonebook/static/js/runtime-main.a4acd5d5.js"
+  }
+]);
